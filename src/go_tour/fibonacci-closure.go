2171e030fc305690ec9f935515c57d2e59ba525a
fmaj7 <dingguanghao@gmail.com>
2017-04-09 09:44:24 -0700

    Optimize fibonacci and place holder images

--- src/go_tour/fibonacci-closure.go
+++ src/go_tour/fibonacci-closure.go
@@ -5,12 +5,10 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	n := 0
-	nPlus1 := 1
+	n, nPlus1 := 0, 1
 	return func() int {
 		output := n
-		n = nPlus1
-		nPlus1 = n + output
+		n, nPlus1 = nPlus1, n+nPlus1
 		return output
 	}
 }
